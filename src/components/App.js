7b5509029918116ef999168462bb41fcb1718277
pallaviraj05 <pallavi05031998@gmail.com>
2021-04-17 23:21:18 +0530

    button classcomponent

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,30 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+	
     constructor(props) {
+		
 		super(props);
+		this.state={
+			isParaShown:false,
+		};
+		this.showPara=this.showPara.bind(this);
 	};
+	
+	
+	showPara(){
+		this.setState({isParaShown:true});
+	}
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				
+				
+				<button id="click" onClick={this.showPara}>Show Paragraph</button>
+				{this.state.isParaShown ? (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>): null }
+					<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+			
     		</div>
     	);
     }
